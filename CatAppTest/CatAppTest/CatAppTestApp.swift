59eaec2d46d828c64283ce5a16c49447c371ca8a
andres <andres@andress-MacBook-Pro.local>
2025-04-04 10:21:48 -0500

    Initial commit

---
+++ CatAppTest/CatAppTest/CatAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppTestApp.swift
+//  CatAppTest
+//
+//  Created by andres on 4/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CatAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
